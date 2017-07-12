5d8c8e14ab485a370f7f12cd6ec8bba8dd05c7bd
mattbit <me@mattbit.com>
2017-07-12 23:50:44 +0200

    Cosmetic improvements to QER

--- condensed-matter/lab/qer.py
+++ condensed-matter/lab/qer.py
@@ -5,37 +5,37 @@ from __future__ import print_function
 import os
 import sys
 import yaml
+import time
 import string
+import random
+import itertools
 from subprocess import Popen, PIPE, STDOUT
 import logging
 
 CONFIG_DIR = 'config'
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
-print("""
+print('''
                     ___           ___
-                   /\__\         /\  \
-      ___         /:/ _/_       /::\  \
-     /\  \       /:/ /\__\     /:/\:\__\
+                   /\__\         /\  \\
+      ___         /:/ _/_       /::\  \\
+     /\  \       /:/ /\__\     /:/\:\__\\
     /::\  \     /:/ /:/ _/_   /:/ /:/  /
    /:/\:\  \   /:/_/:/ /\__\ /:/_/:/__/___
   /:/ /::\  \  \:\/:/ /:/  / \:\/:::::/  /
  /:/_/:/\:\__\  \::/_/:/  /   \::/~~/~~~~
- \:\/:/  \/__/   \:\/:/  /     \:\~~\
-  \::/  /         \::/  /       \:\__\
+ \:\/:/  \/__/   \:\/:/  /     \:\~~\\
+  \::/  /         \::/  /       \:\__\\
    \/__/           \/__/         \/__/
 
        ~ Quantum Espresso Runner ~
-""")
+
+''')
 
 #######################################
 # Setup                               #
 #######################################
-
-# Create the output directory if it does not exist
-if not os.path.exists(OUTPUT_DIR):
-    os.makedirs(OUTPUT_DIR)
 
 # Parse the configuration files
 config_file = os.path.join(CONFIG_DIR, 'config.yaml')
@@ -47,6 +47,10 @@ with open(config_file) as c:
     except yaml.YAMLError:
         log.critical("There is some problem with config.yaml.")
         exit(1)
+
+# Create the output directory if it does not exist
+if not os.path.exists(config['output_dir']):
+    os.makedirs(config['output_dir'])
 
 class Step(object):
     def __init__(self, name, command, input_file, vars={}):
@@ -77,20 +81,23 @@ class StepRunner(object):
         self.out_dir = out_dir
 
     def run(self, step):
-        logging.info("Running step {}".format(step.name))
+        self._print_banner("Running step {}".format(step.name))
 
         output_path = os.path.join(self.out_dir, "{}.out".format(step.name))
         output_file = open(output_path, "w")
         exec_path = os.path.join(self.bin_dir, step.command)
 
         logging.debug("Running command `{}`".format(exec_path))
-        process = Popen(exec_path, stdin=PIPE, stdout=output_file, stderr=STDOUT)
-        process.communicate(step.input)
-        process.wait()
+        process = Popen(exec_path, stdin=PIPE, stdout=output_file)
+        process.stdin.write(step.input)
+        process.stdin.close()
+        self._spinner(process.poll)
 
         if process.returncode != 0:
-            log.critical("Run failed for step {}".format(step.name))
+            logging.critical("Run failed for step {}".format(step.name))
             exit(1)
+
+        print("Done!")
 
     def _print_banner(self, text):
         print("""
@@ -98,6 +105,23 @@ class StepRunner(object):
 = {:<40} =
 ============================================
 """.format(text))
+
+    def _spinner(self, poll):
+        spinner = itertools.cycle(['.  ', '.. ', '...'])
+
+        messages = ["Thinking about spherical cows",
+                    "What about spherical sheeps? ",
+                    "Dreaming about spherical cows"]
+
+        sys.stdout.write(random.choice(messages))
+
+        while poll() is None:
+            sys.stdout.write(spinner.next())
+            sys.stdout.flush()
+            sys.stdout.write('\b\b\b')
+            time.sleep(0.5)
+
+
 
 runner = StepRunner(config['qe_bin_dir'], config['output_dir'])
 
